b2e1ff691dd9dab82135990aa62a1b5418bedbc7
dandanjacob <danieljt.djt@gmail.com>
2025-06-10 18:52:34 -0300

    fix size buttons finances and removed finances content after first search

--- utils.js
+++ utils.js
@@ -19,7 +19,8 @@ export function reloadScreen(step_now) {
 
     if (step_now == 'CONSULTATION') {
         document.getElementById("btn-register").classList.add("hidden");
-        document.getElementById("btn-register-fan").classList.add('hidden')
+        document.getElementById("btn-register-fan").classList.add('hidden');
+        document.getElementById('finances').inner = '';
     }
 
 }
