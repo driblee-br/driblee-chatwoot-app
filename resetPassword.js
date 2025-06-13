e31a26b23d5a757cf64519d4331e05e2333f24da
dandanjacob <danieljt.djt@gmail.com>
2025-06-13 16:47:06 -0300

    fixed reset password

--- resetPassword.js
+++ resetPassword.js
@@ -1,37 +1,41 @@
+import * as main from './main.js';
+import * as utils from './utils.js';
+
 export async function resetPassword() {
     console.log("A funcionalidade de enviar email ainda não foi implementada")
-    const userName = main.getfullUserDataTwomorrow().name
+    const userName = main.getfullUserDataTwomorrow().mainDocument
+    console.log("Main document:", userName)
+    const url = `${main.getHost()}/reset_password/`;
     try {
-        const url = `${main.getHost()}/reset_password/`;
-
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
+                'Content-Type': 'application/json',
                 'ngrok-skip-browser-warning': 'true'
             },
             mode: 'cors',
             credentials: 'omit',
-            body: {
-                userName
-            }
+            body: JSON.stringify({
+                userName: `${userName}`
+            })
         });
-
+        console.log("Response reset:", response)
         if (!response.ok) {
-            if (response.errorMessage) {
+            if (response?.errorMessage) {
                 utils.showNotification(response.errorMessage, 'error')
             } else {
                 utils.showNotification("Erro ao enviar email", 'error')
             }
         } else {
-            if (response.message) {
+            if (response?.message == "Email enviado") {
                 utils.showNotification(response.message, 'success')
             } else {
-                utils.showNotification("Erro ao enviar email", 'success')
+                utils.showNotification(response.message, 'info')
             }
         }
     } catch {
-        utils.showNotification("Erro inesperado.", 'success')
+        utils.showNotification("Erro inesperado.", 'error')
         return
     }
 }
